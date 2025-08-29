9d93cd84005b71cfeea516055e895c584c0c0217
Nitesh <niteshsengar9831@gmail.com>
2025-08-29 11:09:48 +0530

    secound

--- index.js
+++ index.js
@@ -155,7 +155,7 @@ app.use('*', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   console.log(`POST endpoint available at: http://localhost:${PORT}/bfhl`);
